e684aa31f718c3a8858bf81c0f6a1ed8e6622649
shivam <shivamtrivanshi2@gmail.com>
2018-06-15 10:37:42 +0530

    update/edit Request and Delete Request

--- src/app.js
+++ src/app.js
@@ -7,6 +7,17 @@ document.addEventListener("DOMContentLoaded", getPosts);
 //Listen For Add Post
 document.querySelector(".post-submit").addEventListener("click", addPost);
 
+//Listen For Delete Post
+document.querySelector("#posts").addEventListener("click", deletePost);
+
+//Listen For Edit Post
+document.querySelector("#posts").addEventListener("click", editPost);
+
+//Listen For Back to Add State
+document.querySelector(".card-form").addEventListener("click", cancelEdit);
+
+
+
 
 
 function getPosts() {
@@ -30,13 +41,76 @@ function addPost() {
         answer: answer
     }
 
+    //Validate form
+    if(question === "" || answer === "") {
+        ui.showAlert("Please Fill Out All Fields", "alert alert-danger");
+    }else {
 
-    http.post("http://localhost:3000/posts", data)
-    .then((data) => {
-        ui.showAlert("Answer Added", "alert alert-success");
-        ui.clearFields();
-        getPosts();
-    }).catch((err) => {
-        console.log(err);
-    })
+        if(id === "") {
+            //Create Post 
+            http.post("http://localhost:3000/posts", data)
+            .then((data) => {
+                ui.showAlert("Answer Added", "alert alert-success");
+                ui.clearFields();
+                getPosts();
+            }).catch((err) => {
+                console.log(err);
+            });
+        }else {
+            //Update Post
+            http.put(`http://localhost:3000/posts/${id}`, data)
+            .then((data) => {
+                ui.showAlert("Answer Updated", "alert alert-success");
+                ui.clearFields();
+                ui.changeFormState("add");
+                getPosts();
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
+    }
+}
+
+
+
+function deletePost(e) {
+    if(e.target.parentElement.classList.contains("delete")) {
+        const id = e.target.parentElement.dataset.id;
+
+        if(confirm("Are You Sure?")) {
+            http.delete(`http://localhost:3000/posts/${id}`)
+            .then((resData) => {
+                ui.showAlert("Successfully Deleted", "alert alert-success");
+                getPosts();
+            }).catch((err) => {
+                console.log(err);
+            });
+        }   
+    }
+    e.preventDefault();
+}
+
+
+function editPost(e) {
+    if(e.target.parentElement.classList.contains("edit")) {
+        const id = e.target.parentElement.dataset.id;
+        const question = e.target.parentElement.previousElementSibling.previousElementSibling.textContent;
+        const answer = e.target.parentElement.previousElementSibling.textContent;
+
+        const data = {
+            id: id,
+            question: question,
+            answer: answer
+        }
+
+        ui.fillForm(data);
+    }
+    e.preventDefault()
+}
+
+function cancelEdit(e) {
+    if(e.target.classList.contains("post-cancel")) {
+        ui.changeFormState("add");
+    }
+    e.preventDefault();
 }
--- src/ui.js
+++ src/ui.js
@@ -4,6 +4,8 @@ class UI {
         this.posts = document.querySelector("#posts");
         this.questionInput = document.querySelector("#question");
         this.answerInput = document.querySelector("#answer");
+        this.idInput = document.querySelector("#id");
+        this.submitBtn = document.querySelector(".post-submit");
     }
 
     showPosts(posts) {
@@ -12,8 +14,8 @@ class UI {
             output += `
                 <div class="card mb-3">
                     <div class="card-body">
-                        <h4 class="card-title">${post.question}</h4>
-                        <p>${post.answer}</p>
+                        <h4 class="card-title capitalize">${post.question}</h4>
+                        <p class="capitalize">${post.answer}</p>
                         <a href="#" class="edit card-link" data-id="${post.id}">
                         <i class="fa fa-pencil"></i>
                         </a>
@@ -58,6 +60,56 @@ class UI {
         this.questionInput.value = "";
         this.answerInput.value = "";
     }
+
+    fillForm(data) {
+        this.questionInput.value = data.question;
+        this.answerInput.value = data.answer;
+        this.idInput.value = data.id;
+
+        this.changeFormState("edit");
+    }
+
+    changeFormState(type) {
+        if(type === "edit") {
+            this.submitBtn.textContent = "Update Post";
+            this.submitBtn.className = "post-submit btn btn-warning btn-block";
+
+            //create cancel button
+            const button = document.createElement("button");
+
+            //add classes
+            button.className = "post-cancel btn btn-light btn-block mt-2";
+
+            //add text
+            button.appendChild(document.createTextNode("Cancel Edit"));
+            
+            
+
+            //Get parent
+            const container = document.querySelector(".card-form");
+            const endForm = document.querySelector(".end-form");
+
+            //Append Before
+            container.insertBefore(button, endForm);
+        }else {
+            this.submitBtn.textContent = "Save";
+            this.submitBtn.className = "btn btn-primary btn-block post-submit";
+
+            //Remove Cancel btn
+            if(document.querySelector(".post-cancel")) {
+                document.querySelector(".post-cancel").remove();
+            }
+
+            this.clearIdInput();
+
+            this.clearFields();
+        }
+    }
+
+    clearIdInput() {
+        this.idInput.value = "";
+    }
+
 }
 
 
